0dd3a183e0ce57b56f064f6696db4de4f8dd77b1
Nico Ulbricht <nico.ulbricht@live.de>
2018-09-11 17:53:12 +0200

    update  vscode extensions

--- vscode/extensions.sh
+++ vscode/extensions.sh
@@ -2,8 +2,10 @@ declare -a vsCodeExtensions=(
     "alefragnani.project-manager"
     "eamodio.gitlens"
     "esbenp.prettier-vscode"
+    "GitHub.vscode-pull-request-github"
     "mikestead.dotenv"
     "monokai.theme-monokai-pro-vscode"
+    "ms-vscode.Go"
     "PeterJausovec.vscode-docker"
 )
 
