02f16d439d5b9333b4c555b762c49e744f93d166
Nico Ulbricht <nico.ulbricht@live.de>
2019-03-25 15:17:54 +0100

    fix: uncomment install steps

--- install.sh
+++ install.sh
@@ -1,43 +1,43 @@
-# #!/usr/bin/env bash
+#!/usr/bin/env bash
 
-# # ################################
-# # Installing Dependencies
-# # ################################
-# echo "Setting up Dependencies..."
-# ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-# brew bundle
-# sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# ################################
+# Installing Dependencies
+# ################################
+echo "Setting up Dependencies..."
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew bundle
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-# # ################################
-# # Linking dotfiles
-# # ################################
-# echo "Setting up Dotfiles"
-# ln -s .aliases ~/
-# ln -s .functions ~/
-# ln -s .zshrc ~/
+# ################################
+# Linking dotfiles
+# ################################
+echo "Setting up Dotfiles"
+ln -s .aliases ~/
+ln -s .functions ~/
+ln -s .zshrc ~/
 
-# # ################################
-# # Visual Studio Code
-# # ################################
-# echo "Setting up VSCode..."
-# ln -s "/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code" /usr/local/bin/code
-# ./configuration/install-vscode-extensions.sh
+# ################################
+# Visual Studio Code
+# ################################
+echo "Setting up VSCode..."
+ln -s "/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code" /usr/local/bin/code
+./configuration/install-vscode-extensions.sh
 
-# # ################################
-# # ITerm 2
-# # ################################
-# echo "Setting up ITerm 2..."
-# open "./configuration/iterm-colors.itermcolors"
-# defaults write com.googlecode.iterm2 PromptOnQuit -bool false
-# defaults write com.googlecode.iterm2 HideTab -bool true
-# defaults read -app iTerm > /dev/null 2>&1;
+# ################################
+# ITerm 2
+# ################################
+echo "Setting up ITerm 2..."
+open "./configuration/iterm-colors.itermcolors"
+defaults write com.googlecode.iterm2 PromptOnQuit -bool false
+defaults write com.googlecode.iterm2 HideTab -bool true
+defaults read -app iTerm > /dev/null 2>&1;
 
-# # ################################
-# # Spyware & Ads
-# # ################################
-# echo "Setting up Hostfile..."
-# sudo cp /etc/hosts /etc/hosts.backup
-# sudo cp ./configs/hosts /etc/hosts
+# ################################
+# Spyware & Ads
+# ################################
+echo "Setting up Hostfile..."
+sudo cp /etc/hosts /etc/hosts.backup
+sudo cp ./configs/hosts /etc/hosts
 
 # ################################
 # Setup OSX Specifics
