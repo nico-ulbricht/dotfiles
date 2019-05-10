5d9abe5b4195aad473a3a34a93f4da8dd3d6998a
Nico Ulbricht <nico.ulbricht@live.de>
2019-05-10 14:32:13 +0200

    feat: update vscode deps

--- vscode/install.sh
+++ vscode/install.sh
@@ -2,12 +2,16 @@ echo "Setting up VSCode..."
 ln -s "/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code" /usr/local/bin/code
 
 declare -a vsCodeExtensions=(
+    "ckolkman.vscode-postgres"
     "eamodio.gitlens"
-    "esbenp.prettier-vscode"
     "mikestead.dotenv"
     "monokai.theme-monokai-pro-vscode"
+    "ms-kubernetes-tools.vscode-kubernetes-tools"
+    "ms-python.python"
     "ms-vscode.Go"
     "PeterJausovec.vscode-docker"
+    "redhat.vscode-yaml"
+    "yzhang.markdown-all-in-one"
 )
 
 for i in "${vsCodeExtensions[@]}"
