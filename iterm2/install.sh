76cc4e600fdf3c123e6d644f6425232a68410425
Nico Ulbricht <me@nicoulbricht.com>
2020-06-10 14:25:26 +0200

    feat: Add ITerm2 Configuration

---
+++ iterm2/install.sh
@@ -0,0 +1,6 @@
+folder=$(cd "$(dirname "$0")" ; pwd -P)
+
+# Specify the preferences directory
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string $folder
+# Tell iTerm2 to use the custom preferences in the directory
+defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
