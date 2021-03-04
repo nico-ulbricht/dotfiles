7c511f4650da04ba05e4269c8c824102c8999104
Nico Ulbricht <me@nicoulbricht.com>
2021-03-04 13:38:31 +0100

    fix zshrc and ohmyzsh setup

--- .zshrc
+++ .zshrc
@@ -1,4 +1,6 @@
-DIR="$(dirname "$(readlink ~/.zshrc)")"
+source ~/.oh-my-zsh/oh-my-zsh.sh
+
+DIR="$HOME/projects/dotfiles"
 find $DIR -name "*.zsh" -print0 |
 while IFS= read -r -d $'\0' line; do
    source $line
--- zsh/install.sh
+++ zsh/install.sh
@@ -1,3 +1,5 @@
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 folder=$(cd "$(dirname "$0")" ; pwd -P)
 ln -s $folder/.inputrc ~/.inputrc
 ln -s $folder/starship.toml ~/.config/starship.toml
